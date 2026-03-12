77939dd81547cc19bcb6090f159df3d8429fc533
raphael de souza <raphaelcostay@gmail.com>
2026-03-12 12:57:52 -0300

    adicionando seção sobre e modelo 3d da cb 1000r de 2016, entre outros ajustes

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 ScrollReveal().reveal('.hero-text', {
-distance:'60px',
+distance:'100px',
 origin:'left',
-duration:1200
+duration:1300
 })
 
 ScrollReveal().reveal('.hero-img', {
-distance:'60px',
+distance:'100px',
 origin:'right',
-duration:1200
+duration:1300
+})
+ScrollReveal().reveal('.about', {
+distance:'100px',
+origin:'top',
+duration:1300
 })
 
 ScrollReveal().reveal('.card-01', {
-distance:'50px',
+distance:'150px',
 origin:'left',
 duration:1000
 })
 
 ScrollReveal().reveal('.card-02', {
-distance:'50px',
-origin:'bottom',
+distance:'150px',
+origin:'left',
 duration:1000
 })
 
 ScrollReveal().reveal('.card-03', {
-distance:'50px',
+distance:'150px',
 origin:'right',
+duration:1000
+})
+ScrollReveal().reveal('.card-04', {
+distance:'150px',
+origin:'right',
+duration:1000
+})
+
+ScrollReveal().reveal('.card-05', {
+distance:'150px',
+origin:'bottom',
+duration:1000
+})
+
+ScrollReveal().reveal('.card-06', {
+distance:'150px',
+origin:'bottom',
+duration:1000
+})
+ScrollReveal().reveal('.card-07', {
+distance:'150px',
+origin:'bottom',
+duration:1000
+})
+ScrollReveal().reveal('.card-08', {
+distance:'150px',
+origin:'bottom',
 duration:1000
 })
