63014d9377e5caee1885ebfebe93914759213a64
raphael de souza <raphaelcostay@gmail.com>
2026-03-12 00:55:45 -0300

    primeiro commit, iniciando um projeto sobre uma moto absurdamente linda

---
+++ app.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.hero-text', {
+distance:'60px',
+origin:'left',
+duration:1200
+})
+
+ScrollReveal().reveal('.hero-img', {
+distance:'60px',
+origin:'right',
+duration:1200
+})
+
+ScrollReveal().reveal('.card-01', {
+distance:'50px',
+origin:'left',
+duration:1000
+})
+
+ScrollReveal().reveal('.card-02', {
+distance:'50px',
+origin:'bottom',
+duration:1000
+})
+
+ScrollReveal().reveal('.card-03', {
+distance:'50px',
+origin:'right',
+duration:1000
+})
